01f1c3f8c52851dc94709970da79c3d30571b1c4
Ryan Huber <ryan.huber@mac.com>
2015-02-09 01:04:04 -0500

    initial commit

---
+++ db/migrate/20150209054300_create_sections.rb
@@ -0,0 +1,17 @@
+class CreateSections < ActiveRecord::Migration
+  def up
+    create_table :sections do |t|
+      t.string "name"
+      t.integer "position"
+      t.boolean "visible", :default => false
+      t.string "content_type", :index
+      t.text "content"
+      t.integer "page_id"
+      t.timestamps null: false
+    end
+    add_index("sections", "page_id")
+  end
+  def down
+    drop_table :sections
+  end
+end
